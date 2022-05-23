7897efb64ca8b81c05559954346afd92c37754c3
Sletts <Qopster@gmail.com>
2022-05-23 09:45:14 -0700

    Reset

---
+++ Models/Artwork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Artwork
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string ArtWorkName { get; set; } = string.Empty;
+        public Byte[]? ArtWorkData { get; set; }
+    }
+}
