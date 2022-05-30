d6f98d8b96c6993e26ff58c423183679c525e73a
havelpot <quiverisdrawn@gmail.com>
2022-05-30 14:00:47 -0500

    added new homepage picture

--- Poke-Pictionary/src/app/app-routing.module.ts
+++ Poke-Pictionary/src/app/app-routing.module.ts
@@ -1,13 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ArtWorkComponent } from './art-work/art-work.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { PokePicturesComponent } from './poke-pictures/poke-pictures.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: HomePageComponent},
-  {path: 'pokemon', component: PokePicturesComponent}
+  {path: 'pokemon', component: PokePicturesComponent},
+  {path: 'nav-bar', component: NavBarComponent},
+  {path: 'app-art-work', component: ArtWorkComponent}
 ];
 
 @NgModule({
--- Poke-Pictionary/src/app/nav-bar/nav-bar.component.ts
+++ Poke-Pictionary/src/app/nav-bar/nav-bar.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-nav-bar',
+  selector: 'nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
