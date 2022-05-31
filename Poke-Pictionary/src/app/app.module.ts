2b4de1e261530943be47f12f6b6d5b9a6f27e34d
SteveBurgos95 <burgossteve95@gmail.com>
2022-05-31 11:08:57 -0500

    added functional login page

--- Poke-Pictionary/src/app/app.module.ts
+++ Poke-Pictionary/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { PokePicturesComponent } from './poke-pictures/poke-pictures.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { LoginButtonComponent } from './login-button/login-button.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
@@ -19,6 +20,7 @@ import { LoginButtonComponent } from './login-button/login-button.component';
     NavBarComponent,
     HomePageComponent,
     LoginButtonComponent,
+    LoginComponent,
     
   ],
   imports: [
