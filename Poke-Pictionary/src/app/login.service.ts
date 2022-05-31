ddeaf5c1b1ddb83793452cb3bb2a884a9a71ae51
havelpot <quiverisdrawn@gmail.com>
2022-05-31 15:12:49 -0500

    The changes made

---
+++ Poke-Pictionary/src/app/login.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { Observable, throwError, catchError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  token:string = ""
+
+
+  // Login Logic
+  login(userName:string, password:string):Observable<any>{
+    return this.http.post("https://vanquish-p2.azurewebsites.net/api/UserC/Authenticate/", JSON.stringify({userName, password}),
+    // We need to add headers to specify content type
+    {headers: {'Content-Type':'application/json'}}
+    )
+    .pipe(
+      catchError((e) =>{
+        return throwError(e)
+      }
+    ))
+  }
+  // Inject HttpClient into our service
+  constructor(private http:HttpClient) { }
+}
