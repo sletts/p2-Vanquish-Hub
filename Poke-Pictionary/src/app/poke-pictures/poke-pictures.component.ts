63d4d3cdde59a24a6cfb5e77c88a22e24c44746c
havelpot <quiverisdrawn@gmail.com>
2022-06-01 00:45:27 -0500

    Last push for the night

--- Poke-Pictionary/src/app/poke-pictures/poke-pictures.component.ts
+++ Poke-Pictionary/src/app/poke-pictures/poke-pictures.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PokePicturesComponent implements OnInit {
 
+  pokedexMessage:string= "You made a decision!!"
+  whoaMessage:string= "Page work in progress"
+
   constructor() { }
 
   ngOnInit(): void {
